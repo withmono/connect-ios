220faaf409b7abb033fe026a2bdbdd2659887b92
Tristan Tsvetanov <tristan@mono.co>
2021-06-03 17:22:33 -0400

    ditto

--- Sources/ConnectKit/MonoController.swift
+++ Sources/ConnectKit/MonoController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 public class Mono {
 
-    static let sharedInstance: Mono = {
+    public static let sharedInstance: Mono = {
         let instance = Mono()
 
         return instance
