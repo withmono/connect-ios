523a460551b3a05c8601dc5108129b5cc5a8ca0a
Tristan Tsvetanov <tristan@mono.co>
2021-06-03 16:41:14 -0400

    chore: update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,12 +10,8 @@ let package = Package(
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "ConnectIOS",
-            targets: ["ConnectIOS"]),
-        .library(
-            name: "ConnectKit",
-            targets: ["ConnectKit"]),
+        .library(name: "ConnectIOS", targets: ["ConnectIOS"]),
+        .library(name: "ConnectKit", targets: ["ConnectKit"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -24,17 +20,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "ConnectIOS",
-            dependencies: [],
-            path: "Sources/ConnectIOS"),
-        .target(
-            name: "ConnectKit",
-            dependencies: [],
-            path: "Sources/ConnectKit"),
-        .testTarget(
-            name: "ConnectKitTests",
-            dependencies: ["ConnectKit"],
-            path: "Tests/ConnectKitTests"),
+        .target(name: "ConnectIOS", dependencies: []),
+        .target(name: "ConnectKit", dependencies: []),
+        .testTarget(name: "ConnectKitTests", dependencies: ["ConnectKit"]),
     ]
 )
