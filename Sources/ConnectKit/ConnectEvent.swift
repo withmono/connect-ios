68c944e1f713efc30f939efd16860e0293f4f4c6
Tristan Tsvetanov <tristan@mono.co>
2021-06-03 17:10:14 -0400

    fix: remove double implementation of ConnectMetadata

--- Sources/ConnectKit/ConnectEvent.swift
+++ Sources/ConnectKit/ConnectEvent.swift
@@ -6,42 +6,7 @@
 
 import Foundation
 
-public class ConnectMetadata {
-
-    public let type: String // type of event mono.connect.xxxx
-    public let reference: String? // reference passed through the connect setup
-    public let pageName: String? // name of page the widget exited on
-    public let prevAuthMethod: String? // auth method before it was last changed
-    public let authMethod: String? // current auth method
-    public let mfaType: String? // type of MFA the current user/bank requires
-    public let selectedAccountsCount: Int? // number of accounts selected by the user
-    public let errorType: String? // error thrown by widget
-    public let errorMessage: String? // error message describing the error
-    public let institutionId: String? // id of institution
-    public let institutionName: String? // name of institution
-    public let code: String? // the code returned when a link is successful, see https://docs.mono.co/reference/authentication-endpoint
-    public let timestamp: Date // timestamp of the event converted to Date object
-
-    public init(type: String, reference: String? = nil, pageName: String? = nil, prevAuthMethod: String? = nil, authMethod: String? = nil, mfaType: String? = nil, selectedAccountsCount: Int? = nil, errorType: String? = nil, errorMessage: String? = nil, institutionId: String? = nil, institutionName: String? = nil, code: String? = nil, timestamp: Date) {
-        self.type = type
-        self.reference = reference
-        self.pageName = pageName
-        self.prevAuthMethod = prevAuthMethod
-        self.authMethod = authMethod
-        self.mfaType = mfaType
-        self.selectedAccountsCount = selectedAccountsCount
-        self.errorType = errorType
-        self.errorMessage = errorMessage
-        self.institutionId = institutionId
-        self.institutionName = institutionName
-        self.code = code
-        self.timestamp = timestamp
-    }
-
-}
-
 public class ConnectEvent {
-
 
     public let eventName: String // name of event XXXXX
     public let metadata: ConnectMetadata // holds all the related metadata
