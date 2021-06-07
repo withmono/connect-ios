adb37155ffe870636b2a7fe2f2aabfbcd85cfb03
Tristan Tsvetanov <tristan@mono.co>
2021-06-07 08:20:06 -0400

    fix: update library, remove connectios from connectkit

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(name: "ConnectKit", targets: ["ConnectKit", "ConnectIOS"]),
+        .library(name: "ConnectKit", targets: ["ConnectKit"]),
         .library(name: "ConnectIOS", targets: ["ConnectIOS"]),
     ],
     dependencies: [
