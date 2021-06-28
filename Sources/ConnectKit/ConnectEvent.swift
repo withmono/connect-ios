6372e9a1e859cbc894d9b3fabc67529fb42bfbe3
Tristan Tsvetanov <tristan@mono.co>
2021-06-28 15:03:36 -0400

    fix: class reference error

--- Sources/ConnectKit/ConnectEvent.swift
+++ Sources/ConnectKit/ConnectEvent.swift
@@ -13,7 +13,7 @@ public class ConnectEvent {
 
     public init(eventName: String, type: String, reference: String? = nil, pageName: String? = nil, prevAuthMethod: String? = nil, authMethod: String? = nil, mfaType: String? = nil, selectedAccountsCount: Int? = nil, errorType: String? = nil, errorMessage: String? = nil, institutionId: String? = nil, institutionName: String? = nil, timestamp: Date) {
 
-        let data = ConnectMetadata(type: type, reference: reference, pageName: pageName, prevAuthMethod: prevAuthMethod, authMethod: authMethod, mfaType: mfaType, selectedAccountsCount: selectedAccountsCount, errorType: errorType, errorMessage: errorMessage, institutionId: institutionId, institutionName: institutionName, timestamp: timestamp)
+        let data = ConnectData(type: type, reference: reference, pageName: pageName, prevAuthMethod: prevAuthMethod, authMethod: authMethod, mfaType: mfaType, selectedAccountsCount: selectedAccountsCount, errorType: errorType, errorMessage: errorMessage, institutionId: institutionId, institutionName: institutionName, timestamp: timestamp)
 
         self.data = data
         self.eventName = eventName
