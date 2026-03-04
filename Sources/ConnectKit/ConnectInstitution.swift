5d55db94a4c6356407b58ab606a602a3c52588af
victor-mono <victor.o@mono.co>
2026-03-04 12:56:32 +0100

    chore: make `accountNumber` optional in `ConnectInstitution` struct

--- Sources/ConnectKit/ConnectInstitution.swift
+++ Sources/ConnectKit/ConnectInstitution.swift
@@ -11,9 +11,9 @@ public struct ConnectInstitution: Codable {
 
     public let id: String  // institution id in Mono DB
     public let authMethod: String  // enum representing possible authentication methods
-    public let accountNumber: String  // customer account number
+    public let accountNumber: String?  // optional customer account number
 
-    public init(id: String, authMethod: ConnectAuthMethod, accountNumber: String) {
+    public init(id: String, authMethod: ConnectAuthMethod, accountNumber: String? = nil) {
         self.id = id
         self.accountNumber = accountNumber
 
